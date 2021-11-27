4997756f58257d9edfb18f2adb2e2a69d32ea9af
rambip <apero1808@gmail.com>
2021-11-27 16:15:18 +0100

    fixed version of trunk

--- shell.nix
+++ shell.nix
@@ -10,15 +10,43 @@
     targets = [ "wasm32-unknown-unknown" ];
     extensions = ["rust-src" "rust-analysis"];
   });
-  pkgsUnstable =
-    import (fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz) {};
+trunk = with pkgs;
+rustPlatform.buildRustPackage rec {
+  pname = "trunk";
+  version = "0.14.0";
+
+  src = fetchFromGitHub {
+    owner = "thedodd";
+    repo = "trunk";
+    rev = "v${version}";
+    sha256 = "sha256-69MQDIF79pSuaOgZEIqb/ESPQzL7MUiQaJaxPccGxo8=";
+  };
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = if stdenv.isDarwin
+    then [ libiconv CoreServices Security ]
+    else [ openssl ];
+
+  # requires network
+  checkFlags = [ "--skip=tools::tests::download_and_install_binaries" ];
+
+  cargoSha256 = "sha256-3WTxCMNpBmiNbZMHp5BrqTXa1vmE/ZZ/8XbdcfxBfYg=";
+
+  meta = with lib; {
+    homepage = "https://github.com/thedodd/trunk";
+    description = "Build, bundle & ship your Rust WASM application to the web";
+    maintainers = with maintainers; [ freezeboy ];
+    license = with licenses; [ asl20 ];
+  };
+};
 in
   with pkgs;
   stdenv.mkDerivation {
     name = "rust-env";
     buildInputs = [
-      pkgsUnstable.trunk
+      trunk
       rust
       wasm
     ];
 }
+
