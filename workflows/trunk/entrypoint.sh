11fe3ff6f842e9cc9da9b9b49ba2323756a43a61
rambip <apero1808@gmail.com>
2022-07-14 22:16:08 +0200

    test for action

--- workflows/trunk/entrypoint.sh
+++ workflows/trunk/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export HOME=/root
 
 # copy the cached directory from the github vm to the container
 cp -r $HOME/.cargo/registry $HOME/.cargo
 cp -r $HOME/.cache $HOME/.cache
+
+export HOME=/root
 
 rustup toolchain list
 
