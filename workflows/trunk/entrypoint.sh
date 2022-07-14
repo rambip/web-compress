59e3340901d28308c38f67a637907a5142959add
rambip <apero1808@gmail.com>
2022-07-14 22:55:22 +0200

    other tests to understand cache

--- workflows/trunk/entrypoint.sh
+++ workflows/trunk/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-
 export RUSTUP_HOME=/root/.rustup
 
-rustup toolchain list
+du -h $HOME/.cache
+du -h $HOME/.cargo
 
 /root/.cargo/bin/trunk $@
+
+du -h $HOME/.cache
+du -h $HOME/.cargo
