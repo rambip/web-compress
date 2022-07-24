9d0db1e2d3878ec7f369a99adf37db305cc635fe
rambip <apero1808@gmail.com>
2022-07-24 22:45:09 +0200

    working nix expression

--- default.nix
+++ default.nix
@@ -2,11 +2,14 @@ let tools = import ./tools.nix; in with tools;
 
 stdenv.mkDerivation {
     name = "web-compress";
-    src = ./.;
+    unpackPhase = ''
+    cp -r ${./Cargo.toml} Cargo.toml
+    cp -r ${./.cargo} .cargo
+    cp -r ${./Trunk.toml} Trunk.toml
+    cp -r ${./index.html} index.html
+    cp -r ${./src} src
+    '';
     buildInputs = [wasm-bindgen-cli trunk binaryen cargo];
-    configurePhase = ''
-    cargo vendor | 
-    ''
     buildPhase = ''
     trunk build
     '';
